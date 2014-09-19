5e4979fcb26fad6f4054729e0f7dac61e51692fc
wwlei <archive.document.data@gmail.com>
2014-09-19 17:11:11 +0800

    Implementation Of GetSettings

--- jest-common/src/main/java/io/searchbox/indices/settings/GetSettings.java
+++ jest-common/src/main/java/io/searchbox/indices/settings/GetSettings.java
@@ -1,9 +1,41 @@
 package io.searchbox.indices.settings;
 
-/**
- * @author Dogukan Sonmez
- */
+import io.searchbox.AbstractAction;
+import io.searchbox.AbstractMultiTypeActionBuilder;
 
 
-public class GetSettings {
+public class GetSettings extends AbstractAction {
+
+	    private GetSettings(Builder builder) {
+	        super(builder);
+	        this.indexName = builder.index;
+	        setURI(buildURI());
+	    }
+
+	    @Override
+	    public String getRestMethodName() {
+	        return "GET";
+	    }
+
+	    @Override
+	    protected String buildURI() {
+	        StringBuilder sb = new StringBuilder();
+	        sb.append(super.buildURI()).append("/_settings");
+	        return sb.toString();
+	    }
+
+	    public static class Builder extends AbstractMultiTypeActionBuilder<GetSettings, Builder> {
+
+	    	private  String index;
+	    	public Builder(String index)
+	    	{
+	    		this.index = index;
+	    	}
+	        @Override
+	        public GetSettings build() {
+	            return new GetSettings(this);
+	        }
+	    }
+
+	
 }
